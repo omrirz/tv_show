56e63ca80f5e221716ea016470bad4877101596e
Omri Rozenzaft <omri.rozenzaft@gm.com>
2020-03-30 12:38:20 +0300

    comments for character code

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Added
 
 - A character can now tell a random joke.
+- Comments for character code.
 
 ### Changed
 
--- tv_show/character.py
+++ tv_show/character.py
@@ -2,11 +2,14 @@ import pyjokes
 
 
 class Character:
+    """ Common Character functionalities """
     def __init__(self, favorite_phrase=''):
         self.favorite_phrase = favorite_phrase
 
     def speak(self):
+        """ Prints the name of the character and its favorite phrase """
         print(f"{self.__class__.__name__}: {self.favorite_phrase}")
 
     def joke(self):
+        """ Prints the name of the character and a random joke """
         print(f"{self.__class__.__name__} joke:\n{pyjokes.get_joke()}")
